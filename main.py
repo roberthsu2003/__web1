9651c63a6609a66612ba86169590795fc682f24b
徐國堂 <roberthsu2003@gmail.com>
2022-12-23 14:05:36 +0800

    modify

--- main.py
+++ main.py
@@ -1,7 +1,11 @@
-from flask import Flask
+from flask import Flask,render_template
 
 app = Flask(__name__)
 
 @app.route("/")
 def index():
-    return "<h1>Hellow! World!</h1>"
+    return render_template('index.html')
+
+@app.route('/login')
+def login():
+    return render_template('login.html')
